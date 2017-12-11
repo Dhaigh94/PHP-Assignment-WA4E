ac0f3cbbe094afd2c142d9a5eb29bd494a21ec2e
David Haigh <31473893+Dhaigh94@users.noreply.github.com>
2017-12-11 17:32:26 +1100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>David Haigh PHP</title>
+</head>
+<body>
+<h1>David Haigh PHP</h1>
+<p>
+  The SHA256 hash of "David Haigh" is
+<?php
+ echo hash('sha256', 'David Haigh');
+?>
+</p>
+<pre>ASCII ART:
+
+    ********
+    **     **
+    **     **
+    **     **
+    **     **
+    **     **
+    ********
+</pre>
+<a href="check.php">Click here to check the error setting</a>
+<br/>
+<a href="fail.php">Click here to cause a traceback</a>
+</body>
+</html>
